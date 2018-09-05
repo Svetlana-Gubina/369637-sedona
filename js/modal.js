e5f94b510c4bb153c1bd7659536f14737c711642
Svetlana <gubinas83@gmail.com>
2018-09-05 21:48:02 +0300

    Fix bugs:font-family,filter-hover,sort-text,formatting,icon-calendar,iframe

--- js/modal.js
+++ js/modal.js
@@ -5,20 +5,20 @@ var checkout = document.getElementById("checkout");
 var adults = document.getElementById("adults");
 var form = document.querySelector(".hotel-search-form");
 
-    show.addEventListener("click", function (evt) {
-          evt.preventDefault();
-          popup.classList.toggle("modal-show");
-          checkin.focus();
-    });
+  show.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.toggle("modal-show");
+    checkin.focus();
+  });
 
-    form.addEventListener("submit", function (evt) {
-            if(!checkin.value || !checkout.value || !adults.value){
-            evt.preventDefault();
-            }
-    });
-    window.addEventListener("keydown", function (evt) {
-          if (evt.keyCode === 27) {
-            evt.preventDefault();
-            popup.classList.toggle("modal-show");
-            }
-        });
+  form.addEventListener("submit", function (evt) {
+    if(!checkin.value || !checkout.value || !adults.value){
+    evt.preventDefault();
+    }
+  });
+  window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+      popup.classList.toggle("modal-show");
+    }
+  });
